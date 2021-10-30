bb7e8820718c0671560509ee222bf42ed69ee006
thkim7527 <thkim7527@gmail.com>
2021-10-30 18:09:34 +0900

    Add Maze Generator

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,5 +1,8 @@
+import java.util.*
+
 fun main() {
-    val newMaze = Maze(10, 10)
+    val newMaze = Maze(29, 29)
+    newMaze.genMaze()
     newMaze.printMaze()
 }
 
@@ -9,8 +12,8 @@ class Maze(private val height: Int, private val width: Int) {
             true
         }
     }
-    val enter = Location(this, 1, 0).setFalse()
-    val exit = Location(this, 8, 9).setFalse()
+    val enter = Location(this, 1, 0)
+    val exit = Location(this, width - 2, height - 1)
 
     fun printMaze() {
         for(i in 0 until height) {
@@ -26,6 +29,37 @@ class Maze(private val height: Int, private val width: Int) {
             println("")
         }
     }
+
+    fun genMaze() {
+        for(j in 1..height) {
+            for(i in 1..width) {
+                if((j % 2 == 0)&&(i % 2 == 0)) {
+                    val location = Location(this, i - 1, j - 1)
+                    location.setFalse()
+                    location.openWall()
+                }
+            }
+        }
+
+        for(i in 2 until height) {
+            Location(this, width - 2, i - 1).setFalse()
+        }
+
+        for(i in 2 until width) {
+            Location(this, i - 1, height - 2).setFalse()
+        }
+
+        for(i in 0 until height) {
+            Location(this, width - 1, i).setTrue()
+        }
+
+        for(i in 0 until width) {
+            Location(this, i, height - 1).setTrue()
+        }
+
+        enter.setFalse()
+        exit.setFalse()
+    }
 }
 
 class Location(private val maze: Maze, private val x: Int, private val y: Int) {
@@ -35,5 +69,14 @@ class Location(private val maze: Maze, private val x: Int, private val y: Int) {
 
     fun setFalse() {
         maze.map[y][x] = false
+    }
+
+    fun openWall() {
+        val random = Random().nextInt(2)
+        if(random == 0) {
+            val location = Location(maze, x + 1, y).setFalse()//오른쪽 비우기
+        } else {
+            val location = Location(maze, x, y + 1).setFalse() //아래 비우기
+        }
     }
 }
